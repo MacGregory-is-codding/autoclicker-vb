54f546c980c5bd05d253e8182a59bde2fcad6cba
Andrey <48317291+MacGregory-is-codding@users.noreply.github.com>
2020-10-11 22:55:32 +0300

    Before timer adding

---
+++ 000. Before timer adding/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(37, 48)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(117, 24)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(124, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = " Speed (required):"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(221, 45)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(136, 21)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "Unstable clicking"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(221, 72)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(96, 21)
+        Me.CheckBox2.TabIndex = 3
+        Me.CheckBox2.Text = "Shut down"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(37, 160)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(32, 22)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(96, 158)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(32, 22)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(37, 194)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(32, 22)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(96, 192)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(32, 22)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(74, 163)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(16, 17)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "h"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(135, 163)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 17)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "m"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(75, 197)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(15, 17)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "s"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(135, 197)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(26, 17)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "ms"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(37, 131)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(60, 21)
+        Me.RadioButton1.TabIndex = 12
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Time"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(221, 131)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(66, 21)
+        Me.RadioButton2.TabIndex = 13
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Count"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(221, 158)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(149, 22)
+        Me.TextBox5.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(34, 98)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(121, 17)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Optional features:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(221, 209)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(50, 32)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "Eng"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(277, 209)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(50, 32)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "Рус"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(333, 209)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(37, 32)
+        Me.Button3.TabIndex = 18
+        Me.Button3.Text = "?"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(401, 253)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Autoclicker by mg"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ 000. Before timer adding/WindowsApp1/Form1.vb
@@ -0,0 +1,110 @@
+﻿Public Class Form1
+
+    Dim currentLanguage As String       ' Current language variable
+    Dim myCursor As Point               ' Cursor variable (point)
+    Dim clickingStatus = False          ' Current clicking status (on - true, off - false)
+    Dim clickingSpeed As Integer        ' Clicking speed
+
+    Const F8 = 119, F9 = 120            ' Constants for F8 and F9 buttons (F8 - start, F9 - stop); consisting KeyValue (F8 - 119, F9 - 120)
+
+    ' Global key hooking (API using)
+    <Runtime.InteropServices.DllImport("user32.dll")>
+    Shared Function GetAsyncKeyState(ByVal vKey As System.Windows.Forms.Keys) As Short
+    End Function
+    ' Mouse clicking procedure (API using)
+    <Runtime.InteropServices.DllImport("user32.dll")>
+    Private Shared Sub mouse_event(dwFlags As UInteger, dx As UInteger, dy As UInteger, dwData As UInteger, dwExtraInfo As Integer)
+    End Sub
+    ' Cursor position getting function (API using)
+    Public Declare Function GetCursorPos Lib "user32" (ByRef lpPoint As Point) As Integer
+
+    ' Set english language procedure
+    Sub SetEnglish(ByRef currentLanguage As String)
+        If currentLanguage IsNot "english" Then
+            Label1.Text = "Speed (required):"
+            Label2.Text = "h"
+            Label3.Text = "m"
+            Label4.Text = "s"
+            Label5.Text = "ms"
+            Label6.Text = "Optional features:"
+
+            CheckBox1.Text = "Unstable clicking"
+            CheckBox2.Text = "Shut down"
+
+            RadioButton1.Text = "Time"
+            RadioButton2.Text = "Count"
+
+            currentLanguage = "english"
+        End If
+    End Sub
+    ' Set russian language procedure
+    Sub SetRussian(ByRef currentLanguage As String)
+        If currentLanguage IsNot "russian" Then
+            Label1.Text = "Скорость (обязательно):"
+            Label2.Text = "ч"
+            Label3.Text = "м"
+            Label4.Text = "с"
+            Label5.Text = "мс"
+            Label6.Text = "Опционально:"
+
+            CheckBox1.Text = "Нестабильные клики"
+            CheckBox2.Text = "Выключение"
+
+            RadioButton1.Text = "Время"
+            RadioButton2.Text = "Количество"
+
+            currentLanguage = "russian"
+        End If
+    End Sub
+    ' Clicking Function
+    Sub Clicking()
+        While clickingStatus
+            If GetKey() = 120 Then
+                clickingStatus = False
+            End If
+            ' Cursor position getting
+            GetCursorPos(myCursor)
+            ' LMB click with pause
+            mouse_event(&H2, myCursor.X, myCursor.Y, 0, 0)       ' LMB down
+            mouse_event(&H4, myCursor.X, myCursor.Y, 0, 0)       ' LMB up
+            System.Threading.Thread.Sleep(20)                    ' pause
+            Application.DoEvents()
+        End While
+    End Sub
+    ' Get key function
+    Function GetKey() As Integer
+        Dim result As Integer
+        result = 0
+        If (GetAsyncKeyState(119) And &H1) = &H1 Then
+            result = 119
+        End If
+        If (GetAsyncKeyState(120) And &H1) = &H1 Then
+            result = 120
+        End If
+        Return result
+    End Function
+
+    ' Main Form Loading
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        currentLanguage = "none"
+        SetEnglish(currentLanguage)
+
+        ComboBox1.SelectedItem = "100"   ' Set in combobox default speed (100 ms)
+        While True
+            If GetKey() = F8 And Not clickingStatus Then
+                clickingStatus = True
+                Clicking()
+            End If
+        End While
+    End Sub
+
+    ' English language button
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SetEnglish(currentLanguage)
+    End Sub
+    ' Russian language button
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SetRussian(currentLanguage)
+    End Sub
+
+End Class
